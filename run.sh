f7642948aaebbb6d1df1bd8a322eb4122082956d
Ray Andrew <4437323+rayandrew@users.noreply.github.com>
2022-05-25 04:28:17 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -6,10 +6,18 @@
 
 source env/bin/activate
 backup_dir=/tmp/vr-$(expr $6 - 1)/
+if [ -d "$backup_dir" ]; then
+  # remove stale backup dir
+  echo "Removing stale backup dir"
+  rm -rf $backup_dir
+fi
 mkdir $backup_dir
-mv logs $backup_dir 
-mv logs_cntr $backup_dir 
-mv logs_time $backup_dir 
+if [ -d "logs" ]; then
+  mv logs $backup_dir
+  mv logs_cntr $backup_dir
+  mv logs_time $backup_dir
+  echo "logs moved to $backup_dir"
+fi
 mkdir -p logs logs_cntr logs_time
 cd src
 ./consume_driver.py $3 $1 $5 $7 120 360 > tmp_$6.out &
